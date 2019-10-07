0a2261f5a7518d6ab5cf16e2be69216d4f8afdb5
Fredrik Haglund <mejl@fredrikhaglund.se>
2019-10-07 14:05:29 +0200

    Add Content Model/Page Type for Standard Page. Add a view model and a simple view (not using a shared Layout yet).

---
+++ StartingFromScratchLabb/Controllers/StandardPageController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using StartingFromScratchLabb.Models.Pages;
+using StartingFromScratchLabb.Models.ViewModels;
+
+namespace StartingFromScratchLabb.Controllers
+{
+    public class StandardPageController : PageController<StandardPage>
+    {
+        public ActionResult Index(StandardPage currentPage)
+        {
+            var model = new StandardPageViewModel
+            {
+                CurrentPage = currentPage
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ StartingFromScratchLabb/Models/ViewModels/StandardPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StartingFromScratchLabb.Models.Pages;
+
+namespace StartingFromScratchLabb.Models.ViewModels
+{
+    public class StandardPageViewModel
+    {
+        public StandardPage CurrentPage { get; set; }
+
+    }
+}
